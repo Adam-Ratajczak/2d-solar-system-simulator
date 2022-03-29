3771c3147a692f2962935f011d22cf0193f57cc8
Maciej <sppmacd@pm.me>
2022-03-29 21:25:29 +0200

    Enable antialiasing

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,12 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Window/ContextSettings.hpp>
 #include <SFML/Window/Event.hpp>
 #include <iostream>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "2D Solar System Simulator");
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "2D Solar System Simulator", sf::Style::Default, sf::ContextSettings(0, 0, 2));
 
     window.setFramerateLimit(60);
 
