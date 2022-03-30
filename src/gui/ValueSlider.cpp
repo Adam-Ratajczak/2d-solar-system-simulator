35b20de22d36a6630523db97f576573452d442dd
Maciej <sppmacd@pm.me>
2022-03-30 16:54:24 +0200

    GUI: Fix some ValueSlider issues
    - The precision was not properly applied because of negated logarithm
    - The slider's initial value was the average of min and max, but this
    was not considered by textbox.

--- src/gui/ValueSlider.cpp
+++ src/gui/ValueSlider.cpp
@@ -5,29 +5,34 @@
 #include <iostream>
 #include <sstream>
 
+static std::string serialize_value(double value, double step) {
+    std::ostringstream oss;
+    oss << std::fixed;
+    if (step < 1)
+        oss << std::setprecision(std::ceil(-std::log10(step)));
+    else
+        oss << std::setprecision(0);
+    oss << value;
+    return oss.str();
+}
+
 ValueSlider::ValueSlider(Container& parent, double min, double max, double step)
     : Container(parent) {
+
     auto& layout = set_layout<HorizontalBoxLayout>();
     layout.set_spacing(5);
     m_name_textfield = add_widget<Textfield>();
     m_name_textfield->set_size({ 100.0_px, Length::Auto });
     m_slider = add_widget<Slider>(min, max, step);
-    m_slider->on_change = [&](double value) {
-        std::ostringstream oss;
-        oss << std::fixed;
-        if(m_slider->step() < 1)
-            oss << std::setprecision(std::ceil(std::log10(m_slider->step())));
-        else
-            oss << std::setprecision(0);
-        oss << value;
-        m_textbox->set_content(oss.str(), NotifyUser::No);
+    m_slider->on_change = [this](double value) {
+        m_textbox->set_content(serialize_value(value, m_slider->step()), NotifyUser::No);
         if (on_change)
             on_change(value);
     };
     m_textbox = add_widget<Textbox>();
     m_textbox->set_data_type(Textbox::NUMBER);
-    m_textbox->set_content("0");
-    m_textbox->on_change = [&](std::string const& value) {
+    m_textbox->set_content(serialize_value(m_slider->get_value(), m_slider->step()));
+    m_textbox->on_change = [this](std::string const& value) {
         try {
             m_slider->set_value(std::stod(value), NotifyUser::No);
         } catch (...) { ; }
