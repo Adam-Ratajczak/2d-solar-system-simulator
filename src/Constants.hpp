8f0fe96dfb69c4709f218780cf3a63269a4eda52
Maciej <sppmacd@pm.me>
2022-03-31 15:17:44 +0200

    Constants: Remove unused EventListening enum

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -3,10 +3,3 @@
 constexpr double G = 6.67408e-11;
 constexpr double AU = 149.6e6 * 1000;
 constexpr double TIMESTAMP = 3600 * 24;
-
-
-enum class EventListening{
-    ONCLICK,
-    HOVER,
-    ONRELASE
-};
