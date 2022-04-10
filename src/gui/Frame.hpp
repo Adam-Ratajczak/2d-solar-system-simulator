d7fb3d1559ce7534ea99aace0a740ef6bb77e559
Maciej <sppmacd@pm.me>
2022-04-10 11:45:26 +0200

    GUI: Add Frame widget
    It't just a container with some margins and RoundedEdgeRectangleShape as
    a background.

---
+++ src/gui/Frame.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Container.hpp"
+
+class Frame : public Container {
+public:
+    Frame(Container& c)
+        : Container(c) {}
+
+    static constexpr float BorderRadius = 20;
+
+private:
+    virtual void draw(sf::RenderWindow&) const override;
+    virtual float intrinsic_padding() const override { return BorderRadius; }
+};
