c20a65138824ae0129c6897497e0b5888b8e3b88
Maciej <sppmacd@pm.me>
2022-04-10 20:31:17 +0200

    GUI: Port TextButton to sf::String

--- src/gui/TextButton.hpp
+++ src/gui/TextButton.hpp
@@ -9,8 +9,8 @@ class TextButton : public Button {
 public:
     explicit TextButton(Container& c);
 
-    void set_content(std::string content) { m_content = content; }
-    void set_active_content(std::string content) { m_active_content = content; }
+    void set_content(sf::String content) { m_content = std::move(content); }
+    void set_active_content(sf::String content) { m_active_content = std::move(content); }
 
     unsigned get_font_size() const { return m_font_size; }
     void set_font_size(unsigned font_size) { m_font_size = font_size; }
@@ -21,7 +21,8 @@ public:
 private:
     virtual void draw(sf::RenderWindow& window) const override;
 
-    std::string m_content, m_active_content;
+    sf::String m_content;
+    sf::String m_active_content;
     unsigned m_font_size = 20;
     Align m_alignment = Align::CenterLeft;
 };
