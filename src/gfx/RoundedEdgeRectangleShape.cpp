e7ee0d4c7b841d80046060258527bb0cd8c7a741
Maciej <sppmacd@pm.me>
2022-04-17 13:27:45 +0200

    Gfx: Support specifying radius of every corner separately
    (in RoundedEdgeRectangleShape)

--- src/gfx/RoundedEdgeRectangleShape.cpp
+++ src/gfx/RoundedEdgeRectangleShape.cpp
@@ -15,22 +15,29 @@ sf::Vector2f RoundedEdgeRectangleShape::getPoint(std::size_t index) const {
     float subangle = (float)(index % ROUNDED_EDGE_RESOLUTION) / (ROUNDED_EDGE_RESOLUTION - 1) * M_PI_2;
     float angle = subangle + side * M_PI_2;
 
-    float radius = std::min(std::min(m_borderRadius, m_size.x / 2), m_size.y / 2);
+    float radius_top_left = std::min(std::min(m_border_radius_top_left, m_size.x / 2), m_size.y / 2);
+    float radius_top_right = std::min(std::min(m_border_radius_top_right, m_size.x / 2), m_size.y / 2);
+    float radius_bottom_left = std::min(std::min(m_border_radius_bottom_left, m_size.x / 2), m_size.y / 2);
+    float radius_bottom_right = std::min(std::min(m_border_radius_bottom_right, m_size.x / 2), m_size.y / 2);
 
-    sf::Vector2f offset { std::cos(angle) * radius, std::sin(angle) * radius };
+    sf::Vector2f offset;
     sf::Vector2f base;
     switch (side) {
     case 0:
-        base = { m_size.x - radius, m_size.y - radius };
+        base = { m_size.x - radius_bottom_right, m_size.y - radius_bottom_right };
+        offset = { std::cos(angle) * radius_bottom_right, std::sin(angle) * radius_bottom_right };
         break;
     case 1:
-        base = { radius, m_size.y - radius };
+        base = { radius_bottom_left, m_size.y - radius_bottom_left };
+        offset = { std::cos(angle) * radius_bottom_left, std::sin(angle) * radius_bottom_left };
         break;
     case 2:
-        base = { radius, radius };
+        base = { radius_top_left, radius_top_left };
+        offset = { std::cos(angle) * radius_top_left, std::sin(angle) * radius_top_left };
         break;
     case 3:
-        base = { m_size.x - radius, radius };
+        base = { m_size.x - radius_top_right, radius_top_right };
+        offset = { std::cos(angle) * radius_top_right, std::sin(angle) * radius_top_right };
         break;
     default:
         break;
