970a24bbece453f9cc4fc001cf83a94137f80e28
Maciej <sppmacd@pm.me>
2022-04-19 16:14:24 +0200

    Object: Divide attraction magnitude by other object's gravity factor
    This makes most attracting object detection finally working!

--- src/Object.cpp
+++ src/Object.cpp
@@ -61,7 +61,7 @@ void Object::update_forces_against(Object& object) {
     // TODO: Do it properly
     auto this_attraction = attraction_base * object.m_gravity_factor;
     m_attraction_factor -= this_attraction;
-    auto this_attraction_mag = this_attraction.magnitude_squared();
+    auto this_attraction_mag = this_attraction.magnitude_squared() / object.m_gravity_factor;
 
     if (this_attraction_mag > m_max_attraction && object.m_gravity_factor > m_gravity_factor) {
         m_max_attraction = this_attraction_mag;
@@ -71,7 +71,7 @@ void Object::update_forces_against(Object& object) {
     auto other_attraction = attraction_base * m_gravity_factor;
     object.m_attraction_factor += other_attraction;
 
-    auto other_attraction_mag = other_attraction.magnitude_squared();
+    auto other_attraction_mag = other_attraction.magnitude_squared() / m_gravity_factor;
     if (other_attraction_mag > object.m_max_attraction && object.m_gravity_factor < m_gravity_factor) {
         object.m_max_attraction = other_attraction_mag;
         object.m_most_attracting_object = this;
