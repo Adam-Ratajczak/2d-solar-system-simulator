4eb37502dcbc692bc069186fe7220ef784c4c97e
Maciej <sppmacd@pm.me>
2023-05-29 23:18:51 +0200

    EssaGUI: Add header that I forgot to commit previously
    .. breaking CI

--- src/essagui/EssaGUI.hpp
+++ src/essagui/EssaGUI.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
-#include <Essa/GUI/Overlays/MessageBox.hpp>
-
 #include "../Object.hpp"
 #include "../SimulationView.hpp"
 #include "../World.hpp"
 #include "EssaCreateObject.hpp"
 #include "EssaSettings.hpp"
 #include "FocusedObjectGUI.hpp"
+
+#include <Essa/GUI/Overlays/MessageBox.hpp>
+#include <Essa/GUI/Overlays/NotificationWindow.hpp>
+#include <Essa/GUI/Overlays/ToolWindow.hpp>
 #include <Essa/GUI/Widgets/Button.hpp>
 #include <Essa/GUI/Widgets/ColorPicker.hpp>
 #include <Essa/GUI/Widgets/Container.hpp>
@@ -17,7 +19,6 @@
 #include <Essa/GUI/Widgets/TextButton.hpp>
 #include <Essa/GUI/Widgets/Textbox.hpp>
 #include <Essa/GUI/Widgets/Textfield.hpp>
-#include <Essa/GUI/Overlays/ToolWindow.hpp>
 #include <Essa/GUI/Widgets/ValueSlider.hpp>
 #include <Essa/GUI/Widgets/Widget.hpp>
 
@@ -37,6 +38,9 @@ public:
 
     EssaSettings& settings_gui() { return *m_settings_gui; }
 
+    void set_notification_window(GUI::NotificationWindow& notification_window) { m_notification_window = &notification_window; }
+    GUI::NotificationWindow& notification_window() const { return *m_notification_window; }
+
 private:
     World& m_world;
     virtual void update() override;
@@ -45,6 +49,7 @@ private:
     SimulationView* m_simulation_view = nullptr;
     EssaCreateObject* m_create_object_gui = nullptr;
     EssaSettings* m_settings_gui = nullptr;
+    GUI::NotificationWindow* m_notification_window = nullptr;
 
     bool m_draw_forward_simulation = false;
 
