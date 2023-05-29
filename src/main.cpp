31c0ab22c807722d94ebdaf9607decb14df6abd5
Maciej <sppmacd@pm.me>
2023-05-29 22:50:37 +0200

    Build: Update Essa to 548f162
    32 commits:
    - GUI: Fix global events not being passed to custom event handler
    - Build: Build examples in install.sh
    - Build: Check revision only after project is initialized
    - GUI: Improve EventLoop's tick time accuracy
    - GUI: Add FIXME about mouse grabbing
    - GUI: Remove tooltip on MouseLeave efvent
    - GUI: Handle MouseEnter/MouseLeave for overlays
    - GUI: Don't focus overlays if they ignore events
    - LLGL: Add MouseEnter and MouseLeave events
    - GUI: Make main/background WTR an overlay
    - LLGL: Add fmt formatting for llgl::Event
    - GUI: Add option to keep overlay always on bottom
    - GUI: Make tooltips ignore events
    - GUI: Remove redundant position & size from ToolWindow
    - Build: Add git revision as lib's SOVERSION
    - CMake: Set proper rpath for installed targets
    - Import run-all-tests.sh file from old EssaUtil repository
    - Docs: Add directory structure for global installations
    - Docs: Emphasize that release builds are not runnable
    - Util: Add Point::distance(_sq)()
    - Paste assets to prevent cmake directory not found error
    - Shaders: Make a bunch of fixes
    - GUI: Make NotificationWindow ignore all events
    - GUI: Add an option to ignore events by Overlays
    - Shaders: Use diffuse color for ambient
    - GUI: Add a way to override HostWindow event handler
    - GUI: Don't make Container::do_handle_event() final
    - GUI: Move Notifications out of HostWindow
    - Everywhere: Run clang-format
    - GUI: Make Sliders work properly with negative bounds
    - GUI: Support negative numeric values in Textbox
    - Util: Support Point as coords for MultidimensionalArray

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,18 @@ int main() {
     }
 #endif
 
+    auto& notification_window = host_window.open_overlay<GUI::NotificationWindow>();
+    notification_window.set_size({ 200, 500 });
+    notification_window.set_position({ host_window.size().x() / 2 - notification_window.size().x() / 2, 10 });
+    gui.set_notification_window(notification_window);
+
+    host_window.on_event = [&](GUI::Event const& event) -> GUI::Widget::EventHandlerResult {
+        if (auto* resize = event.get<GUI::Event::WindowResize>()) {
+            notification_window.set_position({ resize->new_size().x() / 2 - notification_window.size().x() / 2, 10 });
+        }
+        return GUI::Widget::EventHandlerResult::NotAccepted;
+    };
+
     // Display splash
     auto& splash = host_window.open_overlay<EssaSplash>(gui.settings_gui());
     splash.show_modal();
